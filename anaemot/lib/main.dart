afea3c789e578b7f6907f15373d4529290da588d
zgtall <31428590+zgting@users.noreply.github.com>
2022-04-28 16:47:50 +0800

    Update main.dart

--- anaemot/lib/main.dart
+++ anaemot/lib/main.dart
@@ -38,6 +38,7 @@ class Person {
 }
 
 void main() {
+    WidgetsFlutterBinding.ensureInitialized();//不加这个强制横/竖屏会报错
     // 强制竖屏
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
